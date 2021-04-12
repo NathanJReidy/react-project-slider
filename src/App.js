905d3049f91b047c240bade2df63c2dd1fef280a
Nathan Reidy <nathanjreidy@gmail.com>
2021-04-12 12:14:09 +1000

    Set up css and project file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from "react";
 import Review from "./Review";
-import "./App.css";
 
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+    <main>
+      <section className="container">
+        <h2 className="title">Our Reviews</h2>
+        <article className="review">
+          <img
+            className="img"
+            src="https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg"
+          ></img>
+          <h4>Peter Jones</h4>
+          <p className="role">Intern</p>
+          <p className="desc">Desc</p>
+          <div className="btnContainer">
+            <button className="leftIcon"> v </button>
+            <button className="rightIcon"> m </button>
+          </div>
+          <button className="surpriseBtn">Surprise Me</button>
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default App;
