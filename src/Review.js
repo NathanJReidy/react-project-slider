0e7576f76d2bd2c4bd3278f1f7ecb7e5cfa75917
Nathan Reidy <nathanjreidy@gmail.com>
2021-04-12 12:51:46 +1000

    Added useState for setting next and previous reviews

--- src/Review.js
+++ src/Review.js
@@ -3,7 +3,42 @@ import people from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [reviewNumber, setReviewNumber] = useState(0);
+  const [review, setReview] = useState(people[reviewNumber]);
+  console.log(review);
+  const { id, image, job, name, text } = review;
+
+  return (
+    <article className="review">
+      <img className="img" src={image}></img>
+      <h4>{name}</h4>
+      <p className="role">{job}</p>
+      <p className="desc">{text}</p>
+      <div className="btnContainer">
+        <button
+          className="previousIcon"
+          onClick={() => {
+            setReview(people[reviewNumber - 1]);
+            setReviewNumber(reviewNumber - 1);
+          }}
+        >
+          {" "}
+          <FaChevronLeft />{" "}
+        </button>
+        <button
+          className="nextIcon"
+          onClick={() => {
+            setReview(people[reviewNumber + 1]);
+            setReviewNumber(reviewNumber + 1);
+          }}
+        >
+          {" "}
+          <FaChevronRight />{" "}
+        </button>
+      </div>
+      <button className="surpriseBtn">Surprise Me</button>
+    </article>
+  );
 };
 
 export default Review;
