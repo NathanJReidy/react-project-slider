859205f3aea9d9641c1b7c961b188751012b13f1
Nathan Reidy <nathanjreidy@gmail.com>
2021-05-06 22:16:36 +1000

    Added slide functionality

--- src/Review.js
+++ src/Review.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import people from "./data";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
   const [reviewIndex, setReviewIndex] = useState(0);
-  const { id, name, job, image, text } = people[reviewIndex];
+  const { id, image, name, title, quote } = people[reviewIndex];
 
   const checkNumber = (newIndex) => {
     if (newIndex > people.length - 1) {
@@ -40,6 +40,21 @@ const Review = () => {
     setReviewIndex(checkNumber(randomNumber));
   };
 
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setReviewIndex((oldIndex) => {
+        let index = oldIndex + 1;
+        if (index > people.length - 1) {
+          index = 0;
+        }
+        return index;
+      });
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [reviewIndex]);
+
   return (
     <div className="mainContainer">
       <button className="arrowBtnLeft">
@@ -50,13 +65,32 @@ const Review = () => {
           style={{ color: "white", margin: 0 }}
         />
       </button>
-      <article className="review">
-        <img className="img" src={image}></img>
-        <h4 className="name">{name}</h4>
-        <p className="role">{job}</p>
-        <p className="desc">{text}</p>
-        <FaQuoteRight size={48} style={{ color: "#ba5d2c", margin: 0 }} />
-      </article>
+
+      {people.map((person, personIndex) => {
+        const { id, image, name, title, quote } = person;
+
+        let position = "nextSlide";
+        if (personIndex === reviewIndex) {
+          position = "activeSlide";
+        }
+        if (
+          personIndex === reviewIndex - 1 ||
+          (reviewIndex === 0 && personIndex === people.length - 1)
+        ) {
+          position = "lastSlide";
+        }
+
+        return (
+          <article className={position}>
+            <img className="img" src={image}></img>
+            <h4 className="name">{name}</h4>
+            <p className="role">{title}</p>
+            <p className="desc">{quote}</p>
+            <FaQuoteRight size={48} style={{ color: "#ba5d2c", margin: 0 }} />
+          </article>
+        );
+      })}
+
       <button className="arrowBtnRight">
         <FiChevronRight
           className="nextIcon"
